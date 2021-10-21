eb19a02da3d8018475d02188127111c1d270f88a
shtukarartem <shtukar95@mail.ru>
2021-10-21 19:02:35 +0300

    DOMElevent

---
+++ tasks/general/DOMelement/js/script.js
@@ -0,0 +1,8 @@
+let element = document.getElementById("first-element");
+console.log(element);
+
+element.classList.add("www") //1.1
+element.classList.remove("www")//1.2
+console.log(element.classList.contains("www"))//1.3
+element.classList.toggle("www")//1.4
+console.log(element.classList)
